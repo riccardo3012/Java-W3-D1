47b2ddab54a9335f7d4ae3794f8fa48d8a293417
riccardo3012 <ronca.rcr@gmail.com>
2023-10-16 23:01:26 +0200

    ex 3-4-5

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,5 +7,15 @@ public class Main {
 // ********************************** EX2 ******************************************
 // SELECT * FROM NumeroFattura WHERE Iva = 20%;
 
-    // ********************************** EX3 ******************************************
+// ********************************** EX3 ******************************************
+//SELECT EXTRACT(YEAR FROM "dataFattura") AS anno, COUNT("numeroFattura") AS numerofatture, SUM(importo) AS totale_importo FROM fatture GROUP BY EXTRACT(YEAR FROM "dataFattura")by anno
+    
 
+//********************************** EX4 ******************************************
+//SELECT * FROM prodotto WHERE (YEAR FROM dataAttivazione) = 2017 AND (inproduzione = true OR incommercio = true);
+
+
+//********************************** EX5 ******************************************
+//SELECT EXTRACT(YEAR FROM "dataFattura") AS anno COUNT(*) AS numerofatture FROM fatture WHERE iva = 20 GROUP BY anno ORDER BY anno;
+
+}
